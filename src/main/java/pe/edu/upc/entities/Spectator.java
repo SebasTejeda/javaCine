63bdee53a79207dfe53e7b2bc191aaf5f148f3ef
Sebastian Tejeda <tejedasebastian129@gmail.com>
2024-03-27 12:38:01 -0500

    Se creó la clase Spectator con el nombre de la persona, edad y dinero que tiene

--- src/main/java/pe/edu/upc/entities/Spectator.java
+++ src/main/java/pe/edu/upc/entities/Spectator.java
@@ -1,4 +1,46 @@
 package main.java.pe.edu.upc.entities;
 
 public class Spectator {
+    private String name;
+    private int age;
+    private double has_price;
+
+    public Spectator(String name, int age, double has_price) {
+        this.name = name;
+        this.age = age;
+        this.has_price = has_price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHas_price() {
+        return has_price;
+    }
+
+    public void setHas_price(double has_price) {
+        this.has_price = has_price;
+    }
+
+    @Override
+    public String toString() {
+        return "Spectator{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", has_price=" + has_price +
+                '}';
+    }
 }
