bb44e0d888e38a7bd2e27ebce82ecbc4a784db97
Sebastian Tejeda <tejedasebastian129@gmail.com>
2024-03-27 12:31:12 -0500

    Se crearon las 3 primeras clases

---
+++ src/main/java/pe/edu/upc/entities/Cine.java
@@ -0,0 +1,8 @@
+package main.java.pe.edu.upc.entities;
+
+public class Cine {
+    private String movie;
+    private double price;
+
+
+}
---
+++ src/main/java/pe/edu/upc/entities/Spectator.java
@@ -0,0 +1,4 @@
+package main.java.pe.edu.upc.entities;
+
+public class Spectator {
+}
