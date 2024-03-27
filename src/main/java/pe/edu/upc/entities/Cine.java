dc1e30ffa3a463ff62efb25474f265ae2441dc8f
Sebastian Tejeda <tejedasebastian129@gmail.com>
2024-03-27 12:36:12 -0500

    Se creó la clase Movie con la película de tipo Movie y el precio

--- src/main/java/pe/edu/upc/entities/Cine.java
+++ src/main/java/pe/edu/upc/entities/Cine.java
@@ -1,8 +1,36 @@
 package main.java.pe.edu.upc.entities;
 
 public class Cine {
-    private String movie;
+    private Movie movie;
     private double price;
 
 
+    public Cine(Movie movie, double price) {
+        this.movie = movie;
+        this.price = price;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Cine{" +
+                "movie=" + movie +
+                ", price=" + price +
+                '}';
+    }
 }
